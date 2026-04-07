4e4b52fcae107dac74c71bc8ba298a4062447139
VlT9Gaming <aidan.meli.f32649@mcast.edu.mt>
2026-04-07 09:02:44 +0200

    added firebase

---
+++ src/firebase-config.ts
@@ -0,0 +1,17 @@
+﻿// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB-sIyPZhitGcn32h4oIIYVXxWnMJxfK4k",
+    authDomain: "webgl-765ab.firebaseapp.com",
+    projectId: "webgl-765ab",
+    storageBucket: "webgl-765ab.firebasestorage.app",
+    messagingSenderId: "169556294625",
+    appId: "1:169556294625:web:7c0e76f83ef047be4ff0a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
